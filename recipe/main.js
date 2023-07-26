9c6ed641ab0451e2cb55f42ada7856ae227f1e04
Cathy Dao <cathydao95@gmail.com>
2023-07-26 15:03:18 -0700

    loop through each element in listItems and add eventListener

--- recipe/main.js
+++ recipe/main.js
@@ -3,3 +3,11 @@ let listItems = document.getElementsByTagName('li')
 console.log(listItems)
 
 
+
+// loop through each element in HTMLCollection and add event listener to each element and run function toggleItem when clicked
+for (let i=0; i<listItems.length; i++){
+  listItems[i].addEventListener('click', toggleItem)
+}
+
+
+
