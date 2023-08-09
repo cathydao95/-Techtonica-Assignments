6451c485b11e45b59580e787c3678b4008c49ed2
Cathy Dao <cathydao95@gmail.com>
2023-08-08 17:42:46 -0700

    create js file and add define variables

---
+++ js-game/main.js
@@ -0,0 +1,28 @@
+let playerX = document.querySelector("#xPlayer");
+let playerO = document.querySelector("#oPlayer");
+let characters = document.querySelector(".characterContainer");
+let gameContainer = document.querySelector(".gameContainer");
+// BOXES
+let boxes = document.getElementsByClassName("box");
+
+// set player 1 or 2
+
+let player1 = "";
+let player2 = "";
+
+function setPlayer1(e) {
+  if (e.target.id === "xPlayer") {
+    player1 = "X";
+    player2 = "O";
+  } else {
+    player1 = "O";
+    player2 = "X";
+  }
+  // characters.style.display = "none";
+  // gameContainer.style.display = "flex";
+}
+
+playerX.addEventListener("click", setPlayer1);
+playerO.addEventListener("click", setPlayer1);
+
+//
