790a6ac3f0421c8035855cf4bc773d45444d0345
Cathy Dao <cathydao95@gmail.com>
2023-08-08 18:42:19 -0700

    add checkWinner functionality

--- js-game/main.js
+++ js-game/main.js
@@ -10,6 +10,17 @@ let player1Turn = true;
 let player1 = "";
 let player2 = "";
 
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+];
+
 function setPlayer1(e) {
   if (e.target.id === "xPlayer") {
     player1 = "X";
@@ -26,13 +37,33 @@ playerX.addEventListener("click", setPlayer1);
 playerO.addEventListener("click", setPlayer1);
 
 // IMPLEMENT BOX CLICK AND FUNCTIONS
+function checkWinner(player) {
+  for (let combo of winCombos) {
+    // destructure each element in combo
+    const [a, b, c] = combo;
+    // check boxes array and check if box elements at index(combo) all equal player
+    if (
+      boxes[a].textContent === player &&
+      boxes[b].textContent === player &&
+      boxes[c].textContent === player
+    ) {
+      console.log("winner");
+    } else {
+      console.log("lol doesnt work");
+    }
+  }
+}
+
 function markBox(e) {
   if (player1Turn) {
     console.log("text");
     e.target.textContent = player1;
+    checkWinner(player1);
+
     player1Turn = false;
   } else {
     e.target.textContent = player2;
+    checkWinner(player2);
     player1Turn = true;
   }
 }
