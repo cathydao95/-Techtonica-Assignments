fcf9d39c0cc277d7e7e01fdd1a81c2257bf09b58
Cathy Dao <cathydao95@gmail.com>
2023-08-09 21:25:28 -0700

    set game inactive when out of turns or word solved

--- js-game/wordle.js
+++ js-game/wordle.js
@@ -1,13 +1,15 @@
 // WORDLE
 
 let rows = document.querySelectorAll(".row");
+// turn rows into an array using spread and map through each row to create an array of letters array
 let guessedLettersArray = [...rows].map((row) => {
   return row.querySelectorAll(".letter");
 });
 
+let wordleActive = true;
 let word = "paste";
 let wordSplit = word.split("");
-let userGuess;
+// set current row and index guess to 0
 let currentRowIndex = 0;
 let currentIndexGuess = 0;
 
@@ -15,6 +17,7 @@ console.log(guessedLettersArray[currentRowIndex]);
 
 function checkGuess() {
   let guessedLetters = guessedLettersArray[currentRowIndex];
+  allCorrect = true;
   // check if word matches word
   for (let i = 0; i < wordSplit.length; i++) {
     if (guessedLetters[i].textContent.toLowerCase() === wordSplit[i]) {
@@ -22,38 +25,51 @@ function checkGuess() {
     } else if (
       wordSplit.includes(guessedLetters[i].textContent.toLowerCase())
     ) {
+      allCorrect = false;
       guessedLetters[i].style.backgroundColor = "yellow";
+    } else {
+      allCorrect = false;
     }
   }
   currentRowIndex++;
   currentIndexGuess = 0;
+
+  if (allCorrect) {
+    wordleActive = false;
+    alert("WINNER");
+  } else if (currentRowIndex > 5) {
+    wordleActive = false;
+    alert("out of turns");
+  }
 }
 
 function handleKeyPress(e) {
-  console.log(e.key, currentIndexGuess, currentRowIndex);
-  // use e.key to find out what key is being pressed
-  let key = e.key.toLowerCase();
-  // if key is backspace and currentIndex isnt 0, clear the index before current and decrease currentIndexGuess
-  if (key === "backspace" && currentIndexGuess !== 0) {
-    guessedLettersArray[currentRowIndex][currentIndexGuess - 1].textContent =
-      "";
-    currentIndexGuess--;
-    e.preventDefault();
-  }
+  if (wordleActive) {
+    console.log(e.key, currentIndexGuess, currentRowIndex);
+    // use e.key to find out what key is being pressed
+    let key = e.key.toLowerCase();
+    // if key is backspace and currentIndex isnt 0, clear the index before current and decrease currentIndexGuess
+    if (key === "backspace" && currentIndexGuess !== 0) {
+      guessedLettersArray[currentRowIndex][currentIndexGuess - 1].textContent =
+        "";
+      currentIndexGuess--;
+      e.preventDefault();
+    }
 
-  if (
-    currentIndexGuess < guessedLettersArray[currentRowIndex].length &&
-    key.match(/[a-z]/) &&
-    key !== "backspace" &&
-    key !== "enter"
-  ) {
-    guessedLettersArray[currentRowIndex][currentIndexGuess].textContent = key;
-    currentIndexGuess++;
-    e.preventDefault();
-  }
-  // if user presses enter and current index is at 5, run checkGuess function
-  if (key === "enter" && currentIndexGuess === 5) {
-    checkGuess();
+    if (
+      currentIndexGuess < guessedLettersArray[currentRowIndex].length &&
+      key.match(/[a-z]/) &&
+      key !== "backspace" &&
+      key !== "enter"
+    ) {
+      guessedLettersArray[currentRowIndex][currentIndexGuess].textContent = key;
+      currentIndexGuess++;
+      e.preventDefault();
+    }
+    // if user presses enter and current index is at 5, run checkGuess function
+    if (key === "enter" && currentIndexGuess === 5) {
+      checkGuess();
+    }
   }
 }
 
