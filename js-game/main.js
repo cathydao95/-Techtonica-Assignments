a582e492ffdb4c790059e58b3211f72f4bd51ab6
Cathy Dao <cathydao95@gmail.com>
2023-08-10 22:04:27 -0700

    update button styling

--- js-game/main.js
+++ js-game/main.js
@@ -2,12 +2,26 @@
 let optionsContainer = document.querySelector(".options-container");
 let games = document.querySelectorAll(".game");
 
+let returnBtns = document.querySelectorAll(".returnBtn");
+
 function setGame(e) {
   let game = e.target.classList[1];
-  // console.log(game.classList[1]);
   document.querySelector(`#${game}`).style.display = "flex";
   optionsContainer.style.display = "none";
 }
 games.forEach((game) => {
   game.addEventListener("click", setGame);
+});
+
+// return to game selections
+
+function returnToMain(e) {
+  // get parent node of the return button
+  let game = e.target.parentNode;
+  game.style.display = "none";
+  optionsContainer.style.display = "block";
+}
+
+returnBtns.forEach((btn) => {
+  btn.addEventListener("click", returnToMain);
 });
