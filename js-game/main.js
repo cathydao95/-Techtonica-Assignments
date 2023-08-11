ca791b81035ce6635e0125590e48b6f4cd167f4b
Cathy Dao <cathydao95@gmail.com>
2023-08-10 21:48:02 -0700

    update home modal

--- js-game/main.js
+++ js-game/main.js
@@ -1,13 +1,13 @@
 // GAME OPTIONS CONTAINER
 let optionsContainer = document.querySelector(".options-container");
-let gameOptions = document.querySelectorAll(".game");
-console.log(gameOptions);
+let games = document.querySelectorAll(".game");
 
-// GAME OPTIONS FUNCTIONS
-// function setGame(e) {
-//   console.log(e.targets);
-// }
-
-// gameOptions.forEach((game) => {
-//   game.addEventListener("click", setGame);
-// });
+function setGame(e) {
+  let game = e.target.classList[1];
+  // console.log(game.classList[1]);
+  document.querySelector(`#${game}`).style.display = "flex";
+  optionsContainer.style.display = "none";
+}
+games.forEach((game) => {
+  game.addEventListener("click", setGame);
+});
